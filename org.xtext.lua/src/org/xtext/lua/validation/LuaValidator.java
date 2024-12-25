19e76a9c219fa26d1ae8c995f5125d4f970a8ed3
Martin Armbruster <armbrustermartin@googlemail.com>
2024-12-25 14:42:42 +0100

    Removed unused plugins including their generation.

--- org.xtext.lua/src/org/xtext/lua/validation/LuaValidator.java
+++ org.xtext.lua/src/org/xtext/lua/validation/LuaValidator.java
@@ -9,16 +9,4 @@ package org.xtext.lua.validation;
  * See https://www.eclipse.org/Xtext/documentation/303_runtime_concepts.html#validation
  */
 public class LuaValidator extends AbstractLuaValidator {
-
-//	public static final String INVALID_NAME = "invalidName";
-//
-//	@Check
-//	public void checkGreetingStartsWithCapital(Greeting greeting) {
-//		if (!Character.isUpperCase(greeting.getName().charAt(0))) {
-//			warning("Name should start with a capital",
-//					LuaPackage.Literals.GREETING__NAME,
-//					INVALID_NAME);
-//		}
-//	}
-
 }
