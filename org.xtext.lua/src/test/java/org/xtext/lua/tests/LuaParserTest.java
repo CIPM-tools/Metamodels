17e3750be90267b445a84e2a5ffe9f4c8488151e
Martin Armbruster <armbrustermartin@googlemail.com>
2025-01-05 20:15:56 +0100

    Replaced the Xtend test classes with Java classes.

--- org.xtext.lua/src/test/java/org/xtext/lua/tests/LuaParserTest.java
+++ org.xtext.lua/src/test/java/org/xtext/lua/tests/LuaParserTest.java
@@ -3,10 +3,12 @@ package org.xtext.lua.tests;
 import java.io.IOException;
 import java.nio.file.Paths;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.xtext.lua.LuaParser;
 
 public class LuaParserTest {
+	@Disabled
 	@Test
 	public void minimalParseTest() throws IOException {
 		var resourceSet = new LuaParser().parse(Paths.get("YOUR PATH TO A DIRECTORY WITH LUA CODE"));
